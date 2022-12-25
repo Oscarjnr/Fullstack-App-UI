e423a175184835fc1526cfdbf8e60e64cbc9a191
oscarjnr <yinkaadeyemi121@gmail.com>
2022-12-25 07:02:14 +0100

    component and static list of universities

--- src/app/components/universities/universities-list/universities-list.component.ts
+++ src/app/components/universities/universities-list/universities-list.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { University } from 'src/app/models/university.model';
 
 @Component({
   selector: 'app-universities-list',
   templateUrl: './universities-list.component.html',
   styleUrls: ['./universities-list.component.css']
 })
-export class UniversitiesListComponent {
+export class UniversitiesListComponent implements OnInit {
+
+  universities: University[] = [
+    {
+      id: '1b3422dfss-fg31b-avbdg-h494hfnsjeii',
+      nameOfUniversity: 'University of Ilorin',
+      city: 'Ilorin',
+      state: 'Kwara',
+      nameOfVC: 'Prof. Abolaji Oni',
+      email: 'unilorinedu@gmail.com',
+      contactNumber: 9811116512
+    },
+    {
+      id: '1b34647dfss-2231b-avbdg-h494h35jeii',
+      nameOfUniversity: 'University of Nigeria',
+      city: 'Nsukka',
+      state: 'Enugu',
+      nameOfVC: 'Prof. Udeh Oga',
+      email: 'unnedu@gmail.com',
+      contactNumber: 6838748848333
+    }
+  ];
+  constructor() { }
+  
+  ngOnInit(): void {
+
+  }
 
 }
---
+++ src/app/models/university.model.ts
@@ -0,0 +1,10 @@
+export interface University {
+    id: string;
+    nameOfUniversity: string;
+    city: string;
+    state: string;
+    nameOfVC: string;
+    email: string;
+    contactNumber: number;
+
+}
